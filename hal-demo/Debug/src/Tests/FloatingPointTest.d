c9bf28a191e808d177ba8574389304052420ca48
Tyler <tylermair@cmail.carleton.ca>
2021-11-06 15:12:04 -0400

    remove build from tracking 1

--- hal-demo/Debug/src/Tests/FloatingPointTest.d
+++
@@ -1,4 +0,0 @@
-src/Tests/FloatingPointTest.o: ../src/Tests/FloatingPointTest.c \
- C:/ISIS/MAAE4997B_CDH/hal-demo/../hal//hal/include/hal/boolean.h
-
-C:/ISIS/MAAE4997B_CDH/hal-demo/../hal//hal/include/hal/boolean.h:
