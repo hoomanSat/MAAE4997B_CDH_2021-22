cfbef2f1c0b5dbdf52e850ed29dace70b37b6772
SamDunthorne <52112697+SamDunthorne@users.noreply.github.com>
2022-03-20 13:42:45 -0400

    Cleaned up UART Read

--- hal-demo/src/Tests/SAM_UART_Read_Test.c
+++ hal-demo/src/Tests/SAM_UART_Read_Test.c
@@ -5,7 +5,6 @@
  *      Author: Sam Dunthorne
  */
 
-
 #include <at91/boards/ISIS_OBC_G20/board.h>
 #include <at91/utility/trace.h>
 #include <at91/commons.h>
@@ -28,7 +27,6 @@ void taskSAM_UART_Read_Test(void *arguments) {
 	int UART_Response_Code = 0;
 	unsigned int readSize = 5, i;
 	unsigned char readData[5] = {0};
-	unsigned char writeData[8] = {0};
 	UARTbus bus = *((UARTbus*)arguments);
 
 	while(1) {
@@ -47,53 +45,37 @@ void taskSAM_UART_Read_Test(void *arguments) {
 
 Boolean SAM_UART_Read_Test() {
 	int UART_Response_Code = 0;
-	//unsigned int bus2type = 0;
-	xTaskHandle taskUART0testHandle; //, taskUART2testHandle;
+	xTaskHandle UART0testHandle;
+	xTaskHandle UART2testHandle;
 	static UARTbus UARTtestBus[2] = {bus0_uart, bus2_uart};
 
 	// The iOBC has two different UART inputs on J7, UART0 is pins 10 and 11 (Rx0 & Tx0), and UART2 is pins 13 & 16 (Rx2 & Tx2)
-	// For testing, the baudrate needed to be lowered to 9600, higher speeds produced corrupted data
-	// The timeout is 0x2580 which equals 1 second: time in seconds = HEX(speed/baudrate)
 
 	UARTconfig configBus0 = {.mode = AT91C_US_USMODE_NORMAL | AT91C_US_CLKS_CLOCK | AT91C_US_CHRL_8_BITS | AT91C_US_PAR_NONE | AT91C_US_OVER_16 | AT91C_US_NBSTOP_1_BIT,
 								.baudrate = 115200, .timeGuard = 1, .busType = rs232_uart, .rxtimeout = 0x2580};
 
-	/*
+
 	UARTconfig configBus2 = {.mode = AT91C_US_USMODE_HWHSH  | AT91C_US_CLKS_CLOCK | AT91C_US_CHRL_8_BITS | AT91C_US_PAR_NONE | AT91C_US_OVER_16 | AT91C_US_NBSTOP_1_BIT,
 								.baudrate = 9600, .timeGuard = 1, .busType = rs232_uart, .rxtimeout = 0x2580};
 
 
-
-	//printf("\n This test will receive 4 characters over UART, capitalize them and send them back. \n");
-	//printf(" If you send \"12ab\", you will receive back \"12AB\" on the same bus. \n");
-	//printf("\n Please select a configuration for UART2 (0=RS232 1=RS422): \n");
-	//UTIL_DbguGetIntegerMinMax(&bus2type, 0, 1);
-	if(bus2type != 0) {
-		configBus2.mode = AT91C_US_USMODE_NORMAL | AT91C_US_CLKS_CLOCK | AT91C_US_CHRL_8_BITS | AT91C_US_PAR_NONE | AT91C_US_OVER_16 | AT91C_US_NBSTOP_1_BIT;
-		configBus2.busType = rs422_noTermination_uart;
-	}
-	*/
 	// Both UART peripherals must be started separately as they can use different configurations.
+
 	UART_Response_Code = UART_start(bus0_uart, configBus0);
 	if(UART_Response_Code != 0) {
 		TRACE_WARNING("\n\r UARTtest: UART_start returned %d! \n\r", UART_Response_Code);
 		while(1);
 	}
 
-	printf("Started \n");
-
-	/*
 	UART_Response_Code = UART_start(bus2_uart, configBus2);
-	if(UART_Response_Code != 0) {
-		TRACE_WARNING("\n\r UARTtest: UART_start returned %d! \n\r", UART_Response_Code);
-		while(1);
-	}
+    if(UART_Response_Code != 0) {
+        TRACE_WARNING("\n\r UARTtest: UART_start returned %d! \n\r", UART_Response_Code);
+        while(1);
+    }
 
-	*/
-
-	// Instantiate two separate versions of taskUARTtest and pass different bus-id's as a parameter.
-	xTaskGenericCreate(taskSAM_UART_Read_Test, (const signed char*)"taskUARTtest-0", 1024, (void*)&UARTtestBus[0], 2, &taskUART0testHandle, NULL, NULL);
-	//xTaskGenericCreate(taskSAM_UART_Test, (const signed char*)"taskUARTtest-2", 1024, (void*)&UARTtestBus[1], 2, &taskUART2testHandle, NULL, NULL);
+	// Instantiate two separate versions of UARTtest and pass different bus-id's as a parameter.
+	xTaskGenericCreate(taskSAM_UART_Read_Test, (const signed char*)"UARTtest-0", 1024, (void*)&UARTtestBus[0], 2, &UART0testHandle, NULL, NULL);
+	xTaskGenericCreate(taskSAM_UART_Read_Test, (const signed char*)"UARTtest-2", 1024, (void*)&UARTtestBus[1], 2, &UART2testHandle, NULL, NULL);
 
 
 	printf("\n\n\r");
