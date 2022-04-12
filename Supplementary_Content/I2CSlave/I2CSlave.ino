12da92e7db3970e1ac205408b714f0fccbc66788
SamDunthorne <52112697+SamDunthorne@users.noreply.github.com>
2022-04-11 22:02:34 -0400

    Uploaded Arduino I2C Slave Test

---
+++ Supplementary_Content/I2CSlave/I2CSlave.ino
@@ -0,0 +1,32 @@
+// Slave on the I2C Bus at address 0x41
+// Receives or sends a message on request
+
+#include <Wire.h>
+void setup()
+{
+  Wire.begin(0x41);
+  Wire.setClock(400000);
+  
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
+  Serial.begin(9600);
+}
+void loop()
+{
+  delay(500);
+}
+
+char message;
+void receiveEvent(int howMany)
+{
+  while(0 < Wire.available())
+  {
+    message = Wire.read();
+    Serial.print(message);
+  }
+  Serial.println();
+}
+
+void requestEvent() {
+  Wire.write("Hello iOBC");
+}
