8141b7d94aa5e0f3315607e6e88ab5c1b20e1b63
Tyler <tylermair@cmail.carleton.ca>
2022-04-16 15:34:52 -0400

    final commit again

--- hal-demo/src/Tests/FreeRTOSTest.h
+++ hal-demo/src/Tests/FreeRTOSTest.h
@@ -8,19 +8,6 @@
 #ifndef TESTS_FREERTOSTEST_H_
 #define TESTS_FREERTOSTEST_H_
 
-
-#include <csp/csp_debug.h>
-#include <csp/arch/csp_thread.h>
-#include <csp/arch/csp_clock.h>
-#include <csp/arch/csp_time.h>
-#include <csp/arch/csp_malloc.h>
-#include <csp/arch/csp_queue.h>
-#include <csp/arch/csp_semaphore.h>
-
-#include <hal/boolean.h>
-
-#include <stdlib.h>
-
 Boolean freeRTOSTest();
 
 #endif /* TESTS_FREERTOSTEST_H_ */
